81d021ebbfe62a76d5333236f604a0acb029325d
DESKTOP-BNVQCL4\82109 <amyyzzin@naver.com>
2022-09-28 21:40:59 +0900

    feat : [003-EMAIL-VERIFICATION] CUSTOMER-EMAIL-VERIFICATION.

--- src/main/java/com/tistory/amyyzzin/cms/domain/repository/CustomerRepository.java
+++ src/main/java/com/tistory/amyyzzin/cms/domain/repository/CustomerRepository.java
@@ -1,10 +1,12 @@
 package com.tistory.amyyzzin.cms.domain.repository;
 
 import com.tistory.amyyzzin.cms.domain.model.Customer;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
+    Optional<Customer> findByEmail(String email);
 }
--- src/main/java/com/tistory/amyyzzin/cms/service/SignUpCustomerService.java
+++ src/main/java/com/tistory/amyyzzin/cms/service/SignUpCustomerService.java
@@ -1,10 +1,20 @@
 package com.tistory.amyyzzin.cms.service;
 
+import static com.tistory.amyyzzin.cms.exception.ErrorCode.ALREADY_VERIFY;
+import static com.tistory.amyyzzin.cms.exception.ErrorCode.EXPIRE_CODE;
+import static com.tistory.amyyzzin.cms.exception.ErrorCode.NOT_FOUND_USER;
+import static com.tistory.amyyzzin.cms.exception.ErrorCode.WRONG_VERIFICATION;
+
 import com.tistory.amyyzzin.cms.domain.SignUpForm;
 import com.tistory.amyyzzin.cms.domain.model.Customer;
 import com.tistory.amyyzzin.cms.domain.repository.CustomerRepository;
+import com.tistory.amyyzzin.cms.exception.CustomException;
+import java.time.LocalDateTime;
+import java.util.Locale;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +24,38 @@ public class SignUpCustomerService {
 
     public Customer signUp(SignUpForm form) {
         return customerRepository.save(Customer.from(form));
+    }
+
+    public boolean isEmailExist(String email) {
+        return customerRepository.findByEmail(email.toLowerCase(Locale.ROOT))
+            .isPresent();
+    }
+
+    @Transactional
+    public void verifyEmail(String email, String code) {
+        Customer customer = customerRepository.findByEmail(email)
+            .orElseThrow(() -> new CustomException(NOT_FOUND_USER));
+        if (customer.isVerify()) {
+            throw new CustomException(ALREADY_VERIFY);
+        } else if (customer.getVerificationCode().equals(code)) {
+            throw new CustomException(WRONG_VERIFICATION);
+        } else if (customer.getVerifyExpiredAt().isBefore(LocalDateTime.now())) {
+            throw new CustomException(EXPIRE_CODE);
+        }
+        customer.setVerify(true);
+    }
+
+    @Transactional
+    public LocalDateTime changeCustomerValidateEmail(Long customerId, String verificationCode) {
+        Optional<Customer> customerOptional = customerRepository.findById(customerId);
+
+        if (customerOptional.isPresent()) {
+            Customer customer = customerOptional.get();
+            customer.setVerificationCode(verificationCode);
+            customer.setVerifyExpiredAt(LocalDateTime.now().plusDays(1));
+            return customer.getVerifyExpiredAt();
+        }
+        throw new CustomException(NOT_FOUND_USER);
     }
 
 }
