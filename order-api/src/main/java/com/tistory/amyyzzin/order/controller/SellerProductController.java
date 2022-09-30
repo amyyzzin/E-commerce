3d32085032fc8a1551ba0233a5e59196e40dd0c9
DESKTOP-BNVQCL4\82109 <amyyzzin@naver.com>
2022-09-30 23:42:45 +0900

    feat : [010-PRODUCT_MODIFY] Product & ProductItem Modify
    Product & ProductItem Modify

--- order-api/src/main/java/com/tistory/amyyzzin/order/controller/SellerProductController.java
+++ order-api/src/main/java/com/tistory/amyyzzin/order/controller/SellerProductController.java
@@ -1,14 +1,18 @@
-package com.tistory.amyyzzin.order;
+package com.tistory.amyyzzin.order.controller;
 
 import com.tistory.amyyzzin.config.JwtAuthenticationProvider;
 import com.tistory.amyyzzin.order.domain.product.AddProductForm;
 import com.tistory.amyyzzin.order.domain.product.AddProductItemForm;
 import com.tistory.amyyzzin.order.domain.product.ProductDto;
+import com.tistory.amyyzzin.order.domain.product.ProductItemDto;
+import com.tistory.amyyzzin.order.domain.product.UpdateProductForm;
+import com.tistory.amyyzzin.order.domain.product.UpdateProductItemForm;
 import com.tistory.amyyzzin.order.service.ProductItemService;
 import com.tistory.amyyzzin.order.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,5 +43,23 @@ public class SellerProductController {
 
         return ResponseEntity.ok(ProductDto.from(
             productItemService.addProductItem(provider.getUserVo(token).getId(), form)));
+    }
+
+    @PutMapping
+    public ResponseEntity<ProductDto> updateProduct(
+        @RequestHeader(name = "X-AUTH-TOKEN") String token,
+        @RequestBody UpdateProductForm form) {
+
+        return ResponseEntity.ok(
+            ProductDto.from(productService.updateProduct(provider.getUserVo(token).getId(), form)));
+    }
+
+    @PutMapping("/item")
+    public ResponseEntity<ProductItemDto> updateProductItem(
+        @RequestHeader(name = "X-AUTH-TOKEN") String token,
+        @RequestBody UpdateProductItemForm form) {
+
+        return ResponseEntity.ok(ProductItemDto.from(
+            productItemService.updateProductItem(provider.getUserVo(token).getId(), form)));
     }
 }
