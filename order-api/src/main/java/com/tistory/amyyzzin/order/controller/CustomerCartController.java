be9f92b380b9031f50770f540c94cc1658ad0990
DESKTOP-BNVQCL4\82109 <amyyzzin@naver.com>
2022-10-03 01:24:27 +0900

    feat : [013-ADD-CART] 카트에 대한 추가 구현

---
+++ order-api/src/main/java/com/tistory/amyyzzin/order/controller/CustomerCartController.java
@@ -0,0 +1,31 @@
+package com.tistory.amyyzzin.order.controller;
+
+import com.tistory.amyyzzin.config.JwtAuthenticationProvider;
+import com.tistory.amyyzzin.order.application.CartApplication;
+import com.tistory.amyyzzin.order.domain.product.AddProductCartForm;
+import com.tistory.amyyzzin.order.domain.redis.Cart;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customer/cart")
+@RequiredArgsConstructor
+public class CustomerCartController {
+
+    //임시코드
+    private final CartApplication cartApplication;
+    private final JwtAuthenticationProvider provider;
+
+    @PostMapping
+    public ResponseEntity<Cart> addCart(
+        @RequestHeader(name = "X-AUTH-TOKEN") String token,
+        @RequestBody AddProductCartForm form) {
+        return ResponseEntity.ok(cartApplication.addCart(provider.getUserVo(token).getId(), form));
+    }
+
+}
